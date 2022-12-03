78a52cc078b2a6f9644369360441fc40b21f5318
mohammed-algh <mohammedhalgh@hotmail.com>
2022-12-03 22:21:08 +0300

    Adding unshorten url function

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 from YoutubeAPI import *
+import requests
+
+# method for unshort urls
+def unshorten_url(url):
+    session = requests.Session()  # so connections are recycled
+    resp = session.head(url, allow_redirects=True)
+    return resp.url
+
 
 url = str(input("Enter Youtube link: "))
 
+urlC = unshorten_url(url)
 
-parsed_url = urlparse(url)
+parsed_url = urlparse(urlC)
 
 #get the video id from the url
 videoId = parse_qs(parsed_url.query)['v'][0]
