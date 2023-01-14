f2b05d3854f400e853a6f396d743c555522756d9
mohammed-algh <mohammedhalgh@hotmail.com>
2023-01-14 11:30:02 +0300

    Fixing classifier issue...

--- Machine_Learning_Classifiers.py
+++ Machine_Learning_Classifiers.py
@@ -3,44 +3,57 @@ from Preprocessing import doPreprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
+from sklearn import preprocessing
+import numpy as np
+from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
 
 # Classifiers:   SVM ,  NB ,  LR , KNN , DT , RF
 # Feature extraction: N-Gram , TF-IDF
 
 
+# Read CSV file(Dataset)
+df = pd.read_csv("finalDataset.csv", encoding="utf-8-sig") # read dataset
 
-dataset = pd.read_csv("finalDataset.csv", encoding="utf-8-sig") # read dataset
-df = pd.DataFrame(dataset, columns=["Comment", "Label"]) #create dataframe to manipulate the dataset
+# Encode comment and preparing X
+X = df["Comment"].apply(doPreprocessing)
+cv = CountVectorizer()
+X = cv.fit_transform(X).toarray()
 
-# do preprocessing
-for i in range(len(df)):
-    df["Comment"][i] = doPreprocessing(df["Comment"][i])
-
-# convert labels into numbers
-# 0 for negative
-# 1 for netural
-# 2 for positive
-for i in range(len(df)):
-    df["Label"][i] = df["Label"][i]
-    if df["Label"][i] == "Negitive":
-        df["Label"][i] = 0
-    elif df["Label"][i] == "Netural":
-        df["Label"][i] = 1
-    elif df["Label"][i] == "Positive":
-        df["Label"][i] = 2
+# converting labels to numbers and preparing y
+le = preprocessing.LabelEncoder()
+le.fit(df.Label)
+df["Label"] = le.transform(df.Label)
+y = df["Label"]
 
 
 
+#Preparing the dataset
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.30, random_state=80) # Split data into 70% training and 30% testing
 
-X = df.drop("Label",axis=1) # For Comment column (drop Label column)
-y = df["Label"] # For Label column
+# # GridSearchCV to find best tune
+# param_grid={"C":[0.1,1,100,1000],"kernel":["linear","rbf","poly","sigmoid"],'degree':[1,2,3,4,5,6]}
+# grid = GridSearchCV(SVC(),param_grid)
+# grid.fit(X_train,y_train)
+# print(grid.best_params_)
+# print(grid.score(X_test,y_test))
 
 
+# Building the model
+svmClassf = SVC(kernel="linear",C=1,degree=1) # create the classifier SVM
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.30, random_state=42) # Split data into 70% training and 30% testing
-
-
-svmClassf = SVC(kernel="linear") # create the classifier SVM
+# Training the model
+print("Training the model....\n")
 svmClassf.fit(X_train,y_train) # train the classifier
-y_pred = svmClassf.predict(X_test) # test phase
-print(y_pred) # print the prediction
+
+# evaluate the model and printing the result
+y_pred = svmClassf.predict(X_test)
+accuracyT = metrics.accuracy_score(y_test, y_pred)
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+print("The accuracy score of SVM: ",accuracyT)
+print("-----------------------------------------")
