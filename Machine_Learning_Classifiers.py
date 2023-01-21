fa7a5e0702f74edd689d60ca334a893444a24c14
mohammed-algh <mohammedhalgh@hotmail.com>
2023-01-21 14:59:41 +0300

    Reconstruct the code

--- Machine_Learning_Classifiers.py
+++ Machine_Learning_Classifiers.py
@@ -1,50 +1,74 @@
 import pandas as pd
 import pyarabic.araby
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
 from Preprocessing import doPreprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn import metrics
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from time import time
 
 
 # Classifiers:   SVM ,  NB ,  LR , KNN , DT , RF
 # Feature extraction: N-Gram , TF-IDF
 
+def calculating(classifier, param_grid, X_train, X_test, y_train, y_test):
+    estimators = Pipeline([
+        ("vectorizer", CountVectorizer(preprocessor= doPreprocessing)),
+        ("transformer", TfidfTransformer()),
+        ("classifier", classifier)
+    ])
+    gsc = GridSearchCV(estimators, param_grid, cv=5, n_jobs=-1, verbose=True) # change n_jobs to (your cpu cores - 2) to make your device more comfort
+
+    start_time = time()
+    print("Training the model...")
+    gsc.fit(X_train, y_train)
+    print("Training finished", time() - start_time," Seconds")
+    y_pred = gsc.predict(X_test)
+    best_param = gsc.best_params_
+    best_score = gsc.best_score_
+
+    print_result(y_test, y_pred, best_score, best_param)
+    for param_name in sorted(param_grid.keys()):
+        print("%s: %r" % (param_name, gsc.best_params_[param_name]))
+
+
 # Training function
-def training(classifier, featured_comment, y):
+def training(classifier, featured_comment, y, pip_status):
     X_train, X_test, y_train, y_test = train_test_split(featured_comment, y,test_size=0.30)  # Split data into 70% training and 30% testing
 
 
     if classifier == "SVM":
-        svm(X_train, y_train, X_test, y_test)
+        svm(X_train, y_train, X_test, y_test, pip_status)
 
     elif classifier == "NB":
-        nb(X_train, y_train, X_test, y_test)
+        nb(X_train, y_train, X_test, y_test, pip_status)
 
     elif classifier == "LR":
-        lr(X_train, y_train, X_test, y_test)
+        lr(X_train, y_train, X_test, y_test, pip_status)
 
     elif classifier == "KNN":
-        knn(X_train, y_train, X_test, y_test)
+        knn(X_train, y_train, X_test, y_test, pip_status)
 
     elif classifier == "DT":
-        dt(X_train, y_train, X_test, y_test)
+        dt(X_train, y_train, X_test, y_test, pip_status)
 
-    elif classifier == "rf":
-        rf(X_train, y_train, X_test, y_test)
+    elif classifier == "RF":
+        rf(X_train, y_train, X_test, y_test, pip_status)
 
     else:
         print("Wrong classifier input")
 
 
 # SVM function
-def svm(X_train, y_train, X_test, y_test):
+def svm(X_train, y_train, X_test, y_test, pip_status):
     # # GridSearchCV to find best tune
     # param_grid={"C":[0.1,1,100,1000],"kernel":["linear","rbf","poly","sigmoid"],'degree':[1,2,3,4,5,6]}
     # grid = GridSearchCV(SVC(),param_grid,n_jobs=-1)
@@ -52,87 +76,132 @@ def svm(X_train, y_train, X_test, y_test):
     # print(grid.best_params_)
     # print(grid.score(X_test,y_test))
 
+    classifier = SVC() # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1','l2'],
+            'classifier__kernel': ['linear','rbf'],
+            'classifier__C': [0.1,1,100],
+            'classifier__gamma':[0.1,1,'auto'],
+            'classifier__decision_function_shape': ['ovr','ovo']
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
 
-    # Building the model
-    svmClassf = SVC(kernel="linear",C=1,degree=1) # create the classifier SVM
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
 
-    # Training the model
-    print("Training the model....\n")
-    svmClassf.fit(X_train,y_train) # train the classifier
+    else:
 
-    # evaluate the model and printing the result
-    y_pred = svmClassf.predict(X_test)
-    accuracyT = metrics.accuracy_score(y_test, y_pred)
-    print(confusion_matrix(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
-    print("The accuracy score of SVM: ",accuracyT)
-    print("-----------------------------------------")
+        # Building the model
+        svmClassf = SVC(kernel="linear",C=1,degree=1) # create the classifier SVM
+
+        # Training the model
+        print("Training the model....\n")
+        svmClassf.fit(X_train,y_train) # train the classifier
+
+        # evaluate the model and printing the result
+        y_pred = svmClassf.predict(X_test)
+        accuracyT = metrics.accuracy_score(y_test, y_pred)
+        print(confusion_matrix(y_test,y_pred))
+        print(classification_report(y_test,y_pred))
+        print("The accuracy score of SVM: ",accuracyT)
+        print("-----------------------------------------")
+
+
 
 # Naive Bayes function
-def nb(X_train, y_train, X_test, y_test):
-    pass # Remove pass when you start coding on this classifier
+def nb(X_train, y_train, X_test, y_test, pip_status):
+
+    classifier = SVC()  # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1', 'l2'],
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
+
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
+
+    else:
+        pass
+
 
 # Logistic Regression function
-def lr(X_train, y_train, X_test, y_test):
-    pass # Remove pass when you start coding on this classifier
+def lr(X_train, y_train, X_test, y_test, pip_status):
+
+    classifier = SVC()  # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1', 'l2'],
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
+
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
+
+    else:
+        pass
+
 
 # K-Nearest Neighbors function
-def knn(X_train, y_train, X_test, y_test):
-    pass # Remove pass when you start coding on this classifier
+def knn(X_train, y_train, X_test, y_test, pip_status):
+
+    classifier = SVC()  # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1', 'l2'],
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
+
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
+
+    else:
+        pass
+
 
 # Decision Tree function
-from sklearn import tree
-def dt(X_train, y_train, X_test, y_test):
-    # Building the model
-    dtClassf = tree.DecisionTreeClassifier() # create the classifier DT
+def dt(X_train, y_train, X_test, y_test, pip_status):
 
-    # Training the model
-    print("Training the model....\n")
-    dtClassf.fit(X_train,y_train) # train the classifier
+    classifier = DecisionTreeClassifier()  # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1', 'l2'],
+            'classifier__max_depth': [2, 4, 6, 8, 10],
+            'classifier__min_samples_split': [2, 4, 6, 8, 10],
+            'classifier__min_samples_leaf': [1, 2, 3, 4, 5]
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
 
-    # evaluate the model and printing the result
-    y_pred = dtClassf.predict(X_test)
-    accuracyT = metrics.accuracy_score(y_test, y_pred)
-    print(confusion_matrix(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
-    print("The accuracy score of DT: ",accuracyT)
-    print("-----------------------------------------")
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
+
+    else:
+        pass
+
 
 # Random Forest function
-def rf(X_train, y_train, X_test, y_test):
+def rf(X_train, y_train, X_test, y_test, pip_status):
 
-# Create an instance of the RandomForestClassifier
-    # # For tuning and finding the best parameters and score for Rain Forest classifier
-    # rfClassf = RandomForestClassifier()
-    # param_grid = {
-    # 'n_estimators': [10, 50, 100, 200],
-    # 'max_depth': [None, 5, 10, 20],
-    # 'min_samples_split': [2, 5, 10],
-    # 'min_samples_leaf': [1, 2, 4]
-    # }
-    # grid_search = GridSearchCV(estimator=rfClassf, param_grid=param_grid, scoring='accuracy', cv=5, n_jobs=-1)
 
-    # # Fit the GridSearchCV object to the training data
-    # grid_search.fit(X_train, y_train)
+    classifier = SVC()  # Change SVC() with your classifier
+    if pip_status:
+        param_grid = {
+            'vectorizer__ngram_range': [(1, 1)], #(1, 1), (1, 2), (1, 3), (2, 2), (2, 3), (3, 3) TRY EACH ONE
+            'transformer__use_idf': [False, True],
+            'transformer__norm': ['l1', 'l2'],
+            # add yours parameters here (Note: you must write it like this: 'classifier__{name of the parameter}': [ ]
+        }
 
-    # # Print the best parameters and the best score
-    # print("Best parameters: ", grid_search.best_params_)
-    # print("Best score: ", grid_search.best_score_)
+        calculating(classifier, param_grid, X_train, X_test, y_train, y_test)
 
-    # Building the model
-    rfClassf = RandomForestClassifier(max_depth= None, min_samples_leaf= 2, min_samples_split= 4, n_estimators= 100)
-
-    # Training the model
-    print("Training the model....\n")
-    rfClassf.fit(X_train,y_train) # train the classifier
-
-    # evaluate the model and printing the result
-    y_pred = rfClassf.predict(X_test)
-    accuracyT = metrics.accuracy_score(y_test, y_pred)
-    print(confusion_matrix(y_test,y_pred))
-    print(classification_report(y_test,y_pred))
-    print("The accuracy score of RainForest: ",accuracyT)
-    print("-----------------------------------------")
+    else:
+        pass
 
 
 # preparing the dataset
@@ -141,7 +210,7 @@ def prepare():
     df = pd.read_csv("finalDataset.csv", encoding="utf-8-sig") # read dataset
 
     # Encode comment and preparing X
-    X = df["Comment"].apply(doPreprocessing)
+    X = df["Comment"]
 
     # converting labels to numbers and preparing y
     le = preprocessing.LabelEncoder()
@@ -151,26 +220,50 @@ def prepare():
     return X,y
 
 
+
+def print_result(y_test, y_pred, best_score, best_param):
+    print(confusion_matrix(y_test,y_pred))
+    print(classification_report(y_test,y_pred))
+    print("The accuracy score of model: ",metrics.accuracy_score(y_test, y_pred))
+    print("-----------------------------------------")
+    # Print the best parameters and the best score
+    print("Best score: ", best_score)
+    print("Best parameters  for the given ngram : ")
+
+
+
+
 # TF-IDF feature extraction function
 def tfidf(classifier):
     X, y = prepare() # call prepare function
     tfidf_vectorizer = TfidfVectorizer(tokenizer=pyarabic.araby.tokenize, use_idf=True)
     featured_comment = tfidf_vectorizer.fit_transform(X)
-    training(classifier, featured_comment, y) # call training function
+    pip_status = False
+    training(classifier, featured_comment, y, pip_status) # call training function
 
 # N-Grams feature extraction function
-def n_gram(classifier, n):
+def n_gram(classifier, n, m):
     X, y = prepare() # call prepare function
-    vectorizer = CountVectorizer(ngram_range=(n,n))
+    vectorizer = CountVectorizer(ngram_range=(n,m))
     featured_comment = vectorizer.fit_transform(X)
-    training(classifier, featured_comment, y) # call training function
+    pip_status = False
+    training(classifier, featured_comment, y, pip_status) # call training function
+
+
+def pipeline(classifier):
+    X, y = prepare()
+    pip_status = True
+    training(classifier,X,y, pip_status)
+
 
 # This function to start the process of training
-def start(classifier, feature_extraction, n):
+def start(classifier, feature_extraction, n, m):
     if feature_extraction == "ngram":
-        n_gram(classifier,n)
+        n_gram(classifier, n, m)
     elif feature_extraction == "tfidf":
         tfidf(classifier)
+    elif feature_extraction == "pipeline":
+        pipeline(classifier)
     else:
         print("Wrong feature extraction input")
 
@@ -178,4 +271,4 @@ def start(classifier, feature_extraction, n):
 # First parameter choose one of these classifiers: SVM, NB, LR, KNN, DT, RF
 # Second parameter choose one of these feature extraction: ngram, tfidf
 # Third parameter enter n-gram level: 1, 2, 3 (this parameter will be ignored if TF-IDF choosen, but must enter any number)
-start("rf","ngram",1)
+start("DT","pipeline",1,2)
